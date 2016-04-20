5b18914ff6d2d82c3251c52bb57d0c7c0f2fbd4d
DragScorpion <dragscorpion77@gmail.com>
2016-04-20 18:27:50 +0200

    0.1.1 item Update

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -2,7 +2,7 @@ data:extend({
 {
     type = "item",
     name = "CopperChest",
-    icon = "__DragTech__/graphics/group/GTstorage.png",
+    icon = "__DragTech__/graphics//icons/CopperChest.png",
     flags = {"goes-to-quickbar"},
     subgroup = "GTstorage",
     order = "h[DragTech]-b[CopperChest]",
